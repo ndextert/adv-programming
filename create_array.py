4b7e5003cbf94c9092591001bc4006e3edfad37d
nicholson <n.dexter007@gmail.com>
2022-12-21 11:42:57 +0800

    abc

---
+++ create_array.py
@@ -0,0 +1,8 @@
+import numpy as np
+
+numbers_1=np.array([2,3,2,8,9,11]) #single dimension array
+
+numbers_2=np.array([[1,2,3],[5,10,11]]) #multi dimension array
+
+print(numbers_1)
+print(numbers_2)
